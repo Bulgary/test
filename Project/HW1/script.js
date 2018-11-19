10b0909377d7da374f9a81ef970941301e3c89a8
BulhakovYevhen <bulhakov.y.o@gmail.com>
2018-11-20 00:28:24 +0200

    Assignment 1

---
+++ Project/HW1/script.js
@@ -0,0 +1,28 @@
+var money;
+var time;
+
+money = +prompt ("Ваш бюджет на месяц?");
+time = +prompt ("Введите дату в формате YYYY-MM-DD");
+console.log (typeof(money));
+console.log (time);
+
+var appData = {
+    budget: money,
+    timeData: time
+};
+
+var theMostValuableExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+var howMuch = prompt("Во сколько обойдется?");
+var expenses = {
+    theMostValuableExpenses : howMuch
+};
+
+var optionalExpenses = {};
+
+var income = [];
+
+var budgetPerDay = money / 30;
+alert ("бюджет на 1 день " + budgetPerDay);
+
+
+
